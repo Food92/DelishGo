bf92358b036435df1f9fdf41e74f43c868d65681
foodf <fo.fontus@duocuc.cl>
2026-05-05 23:07:57 -0400

    Nueva funcionalidad en microservicio Cliente y la creacion CRUD de cliente

--- cliente-mscv/src/main/java/com/delishGo_MSCVs/cliente_mscv/repositories/ClienteRepository.java
+++ cliente-mscv/src/main/java/com/delishGo_MSCVs/cliente_mscv/repositories/ClienteRepository.java
@@ -1,4 +1,10 @@
 package com.delishGo_MSCVs.cliente_mscv.repositories;
 
-public interface ClienteRepository {
+import com.delishGo_MSCVs.cliente_mscv.models.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    Optional<Cliente> findByCorreo(String run);
 }
